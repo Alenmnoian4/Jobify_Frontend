2cb8ec527a20a58f2f718ad81a3d070f82ccee53
MARY SANNI <marysanni@MARYs-MacBook-Pro.local>
2023-01-24 19:45:57 -0600

    changed create.js

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,4 +1,4 @@
-import './index.css';
+import '../index.css';
 
 function Create (){
   return (
@@ -6,22 +6,22 @@ function Create (){
       <h2>Job Information:</h2>
 <form className="createForm" action="/create" method="post">
 <label for="role">Role:</label>
-  <input type="text" name="role" placeholder="Enter Job Role Here" id=""/><br> </br>
-  <label for="details">Details:</label> <br> </br>
-   <textarea name="details" id="" cols="30" rows="10"></textarea> <br> </br><br> </br>
+  <input type="text" name="role" placeholder="Enter Job Role Here" id=""/><br/> <br/>
+  <label for="details">Details:</label> <br/> <br/>
+   <textarea name="details" id="" cols="30" rows="10"></textarea> <br/> <br/><br/> <br/>
     
    <label for="Location">Location:</label>
-  <input type="text" name="location" placeholder="Enter Location Here" id=""/><br> </br>
+  <input type="text" name="location" placeholder="Enter Location Here" id=""/><br/> <br/>
   
-  <label for="On-site">On-site/Remote:</label><br> </br>
+  <label for="On-site">On-site/Remote:</label><br/> <br/>
   <select name="onsite" id="">
     <option value="On-site">On-site</option>
       <option value="Remote">Remote</option> 
       <option value="Hybrid">Hybrid</option>
       </select>
-    <br> </br>
+      <br/> <br/>
     <label for="appURL">Job url:</label>
-  <input type="url" name="appURL" placeholder="Enter Job url Here" id=""/><br> </br>
+  <input type="url" name="appURL" placeholder="Enter Job url Here" id=""/><br/> <br/>
     <label for="type">Type:</label>
     <select name="type" id="">
       <option value="Full-time">Full-time</option>
@@ -32,16 +32,16 @@ function Create (){
       <option value="Internship">Internship</option>
       <option value="Apprenticeship">Apprenticeship</option>
       <option value="Other">Other</option>
-    </select>  <br> </br>  <br> </br>
-    <label for="salary">Salary:</label><br> </br>
+    </select>  <br/> <br/><br/> <br/>
+    <label for="salary">Salary:</label><br/> <br/>
   <select name="salary" id="">
     <option value="85k">50k-100k</option>
       <option value="120k">100k-150k</option>
       <option value="180k">150k-200k</option>
    </select>
-     <br> </br>
-     <br> </br>
-     <br> </br>
+   <br/> <br/>
+   <br/> <br/>
+   <br/> <br/>
      <input type="Submit"/>
       </form>
       </div>
