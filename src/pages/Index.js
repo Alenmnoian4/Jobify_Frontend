3d00a54e76ee257e904d31436080035faf09005b
Alenmnoian4 <alenmnoian4@gmail.com>
2023-01-24 20:37:41 -0500

    Index Updated

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,8 +1,5 @@
-import { useLoaderData, Form } from "react-router-dom";
-import { ExternalLink } from "react-external-link";
 import "../index.css";
-import { useState } from "react";
-import { Form, Link, useLoaderData } from "react-router-dom"
+import { Link, useLoaderData } from "react-router-dom"
 
 function Index(props) {
     const posts = useLoaderData()
@@ -12,16 +9,12 @@ function Index(props) {
                 <h2>Jobify Posts</h2>
             <div className="index">
             {posts.map(post => (
-                <div className="post">
-                    <Link to={`/${post._id}`} className="post"></Link>
+                <Link><div className="post">
+                     to={`/${post._id}`} className="post"
                         <h2>{post.role}</h2>
                         <h2>{post.details}</h2>
-                        <h2>{post.location}</h2>
-                        <h2>{post.onsite}</h2>
-                        <h2>{post.appURL}</h2>
-                        <h2>{post.location}</h2>
-                        <h2>{post.salary}</h2>
-                </div>
+                        <h2>{post.location}</h2>        
+                </div></Link>
             ))}
             </div>
         </div>
