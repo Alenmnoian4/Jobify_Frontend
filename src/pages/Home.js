6a7acdeed1088f617290159d06af5aa99cd15e46
Ramon Hernandez <ramongh331@gmail.com>
2023-01-25 21:41:59 -0500

    began styling Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,18 +5,20 @@ export default function Home (props) {
 const posts = useLoaderData()
 
     return (
-        <section>
-            <section className="homeText">
-                <h2>A Place For All Your Job Posts</h2>
-                <p>We provide a central location for all job posts. Input the job's details and let us hold onto it for you. </p>
-            </section>
-            <section className="homeGraphic">
-                <img src="#" alt="something" />
+        <section className="homePage">
+            <section class="homeBody">
+                <section className="homeText">
+                    <h2>A Place For All Your <br/>Job Posts</h2>
+                    <p>We provide a central location for all job posts. Input the job's details and let us hold onto it for you. </p>
+                </section>
+                <section className="homeGraphic">
+                    <img src="https://i.imgur.com/SQkvSRO.png" alt="something" />
+                </section>
             </section>
             <section className="additionsSect">
                 <h3>Recent Additions</h3>
                 <section className="jobCards">
-                    {posts.map((post) => (
+                    {posts.slice(-4).map((post) => (
                          <div className="jobCard">
                         <h4>{post.role}</h4>
                         <p>{post.type}</p>
